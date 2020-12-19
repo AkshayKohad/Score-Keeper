6bfddb486fb13d307c9a41c868f13a3c87f40cf9
AkshayKohad <65406900+AkshayKohad@users.noreply.github.com>
2020-12-19 17:13:58 +0530

    Update scorekeeper.js

--- scorekeeper.js
+++ scorekeeper.js
@@ -1,58 +1,61 @@
-const displayplayer1=document.querySelector("#displayer1")
-const displayplayer2=document.querySelector("#displayer2")
-const player1button=document.querySelector("#player1button")
-const player2button=document.querySelector("#player2button")
+const player1={
+	score:0,
+	display:document.querySelector("#displayer1"),
+	button:document.querySelector("#player1button")
+}
+
+const player2={
+	score:0,
+	display:document.querySelector("#displayer2"),
+	button:document.querySelector("#player2button")
+}
+
+function UpdateScores(player,opponent)
+{
+	if(!playgame)
+	{
+		player.score++
+		if(player.score===matchpoint)
+		{
+			playgame=true
+			player.display.classList.add("winner")
+			opponent.display.classList.add("loser")
+			player.button.disabled=true
+			opponent.button.disabled=true
+		}
+	}
+	player.display.textContent=player.score
+}
+
+
+
 const resetbutton=document.querySelector("#reset")
 const ChangeMatchpoint=document.querySelector("#playto")
-let player1=0
-let player2=0
+
 
 let matchpoint=3
 let playgame=false
 
-player1button.addEventListener("click",()=>{
-	if(!playgame)
-	{
-		player1++
-		if(player1===matchpoint)
-		{
-			playgame=true
-			displayplayer1.classList.add("winner")
-			displayplayer2.classList.add("loser")
-			player1button.disabled=true
-			player2button.disabled=true
-		}
-	}
-	displayplayer1.textContent=player1
+player1.button.addEventListener("click",()=>{
+	UpdateScores(player1,player2)
 })
 
 
-player2button.addEventListener("click",()=>{
-	if(!playgame)
-	{
-		player2++
-		if(player2===matchpoint)
-		{
-			playgame=true
-			displayplayer2.classList.add("winner")
-			displayplayer1.classList.add("loser")
-			player1button.disabled=true
-			player2button.disabled=true
-		}
-	}
-	displayplayer2.textContent=player2
+player2.button.addEventListener("click",()=>{
+	UpdateScores(player2,player1)
 })
 
 function reset(){
 	playgame=false
-	player1=0
-	player2=0
-	displayplayer1.textContent=0
-	displayplayer2.textContent=0
-	displayplayer1.classList.remove("winner","loser")
-	displayplayer2.classList.remove("winner","loser")
-	player1button.disabled=false
-	player2button.disabled=false
+	for(let player of [player1,player2])
+	{
+		player.score=0
+	player.display.textContent=0
+	player.display.classList.remove("winner","loser")
+	player.button.disabled=false
+	
+	}
+	
 }
 resetbutton.addEventListener("click",reset)
 
@@ -60,5 +63,3 @@ resetbutton.addEventListener("click",reset)
  	matchpoint=parseInt(this.value)
  	reset()
  })
- 
-
